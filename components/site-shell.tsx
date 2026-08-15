36a55a95993abba25719c598fbae81d041b8b72b
kirtijpatel <kirtijpatel00@gmail.com>
2026-08-15 13:28:19 -0400

    Shorten header training button label

--- components/site-shell.tsx
+++ components/site-shell.tsx
@@ -47,7 +47,7 @@ export function Header() {
         <a className="instagram" href="https://www.instagram.com/" target="_blank" rel="noreferrer" aria-label="Instagram">
           <span className="instagram-logo" aria-hidden="true" />
         </a>
-        <Link className="button button-small" href="/request-training">Request Training / Contact</Link>
+        <Link className="button button-small" href="/request-training">Request Training</Link>
       </nav>
     </div>
   </header>;
